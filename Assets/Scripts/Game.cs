866f88b5605bcdc3754f318006fe08cb7abff8c5
RoyalHugo <hugo.imperat@gmail.com>
2024-11-14 15:52:54 +0100

    Classe Game.cs
    Création de la classe, de quelques méthodes de celle-ci

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class Game
+{
+    List<Player> Players;
+
+    public Game(List<Player> players)
+    {
+        this.Players = players;
+    }
+
+    public void Buy() // J'attends les cartes + peut-être mettre en paramètre une carte, du coup on ajoute direct au deck du joueur, en vérifiant d'abord la disponibilité
+    {
+
+    }
+
+    public bool CheckEnd() // Faut voir si il faut return le joueur plus qu'un bool, ça rendrait la tâche plus simple
+    {
+        foreach (Player p in Players)
+        {
+            if (p.Coins == 20)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
