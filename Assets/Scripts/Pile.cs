8090fdcdc6e523335073a5a19c57362992c8d002
Nathan <ncuenidolis@gmail.com>
2024-11-14 15:40:19 +0100

    Changed cards and pile class file names, added test script

---
+++ Assets/Scripts/Pile.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pile
+{
+    //Attributes
+    public List<Cards> pile = new List<Cards>();
+    private bool isEmpty { get; set; } = false;
+
+    public Pile(List<Cards> Pile)//Any pile as an argument (game pile, player pile ...)
+    {
+        pile = Pile;
+    }
+
+    public bool CheckCards()//Check how many cards are remaining
+    {
+        if (pile.Count == 0)
+        {
+            isEmpty = true;
+        }
+        return isEmpty;
+    }
+
+    public void GetEachCards()//Gets all cards
+    {
+        foreach (var card in pile)
+        {
+            Debug.Log(card.GetName());
+        }
+    }
+}
