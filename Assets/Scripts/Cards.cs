d3b2b49a0c1a50ee1d703d164e8457eba6b2ed9e
Sarah N <sarahniederits@gmail.com>
2024-11-17 00:31:41 +0100

    Fixed Namespace and Cards.cs

--- Assets/Scripts/Cards.cs
+++ Assets/Scripts/Cards.cs
@@ -1,22 +1,30 @@
 using System;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Card", menuName = "Miniville/Card")]
-public class Cards : ScriptableObject
+namespace Miniville
 {
-    public CardsInfo cardsInfo;
-    public Sprite Visual;
+
+    [CreateAssetMenu(fileName = "New Card", menuName = "Miniville/Card")]
+    public class Cards : ScriptableObject
+    {
+        
+
+        public CardsInfo cardsInfo;
+        public Sprite Visual;
+    }
+
+    [Serializable]
+    public struct CardsInfo
+    {
+        //Attributes with accessors
+        public int Id;
+        public string Color;
+        public int Cost;
+        public string Name;
+        public string Effect;
+        public int Dice;
+        public int Gain;
+    }
+    
 }
 
-[Serializable]
-public struct CardsInfo
-{
-    //Attributes with accessors
-    public int Id;
-    public string Color;
-    public int Cost;
-    public string Name;
-    public string Effect;
-    public int Dice;
-    public int Gain;
-}
